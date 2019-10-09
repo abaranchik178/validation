7b0ded9393098b6be635793fa696de6ddca8a786
Andrey B <abaranchik178@gmail.com>
2019-10-09 04:27:07 +0300

    add array validator test

---
+++ tests/ArrayValidatorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace abaranchik178\validation\tests;
+
+use abaranchik178\validation\ArrayValidator;
+use abaranchik178\validation\rules\Email;
+use PHPUnit\Framework\TestCase;
+
+class ArrayValidatorTest extends TestCase
+{
+    /**
+     * @dataProvider validateEmailProvider
+     */
+    public function testValidateEmail($email, $isValid)
+    {
+        $validator = new ArrayValidator();
+        $validator->addRule('some_email', new Email() );
+        $result = $validator->validate([
+            'some_email' => $email
+        ]);
+        $this->assertSame($result, $isValid);
+    }
+    
+    public function validateEmailProvider()
+    {
+        return [
+            ['mail@dfg.ru', true],
+            ['1mail@g.com', true],
+
+            ['mail@@.com', false],
+            ['345', false]
+        ];
+    }
+}
