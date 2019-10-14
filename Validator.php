667799797fc16a858d145236469f7cf6e92fbae3
Andrey B <abaranchik178@gmail.com>
2019-10-15 02:50:08 +0300

    fix bags? little refactor

--- Validator.php
+++ Validator.php
@@ -4,12 +4,58 @@
 namespace abaranchik178\validation;
 
 
-interface Validator
+abstract class Validator
 {
-    public function addRule(string $fieldName, ValidateRule $validateRule);
-    public function validate($data);
-    public function getErrors(string $fieldName = null);
-    public function isHasErrors();
-    public function getErrorsMessages(string $fieldName);
-    public function getErrorsMessagesAsString(string $fieldName);
+    protected $rules;
+    protected $errorsCollection;
+
+    abstract public function validate($data);
+    
+    public function __construct()
+    {
+        $this->errorsCollection = new ValidateErrorsCollection();
+    }
+
+    public function addRule(string $fieldName, ValidateRule $validateRule)
+    {
+        if ( ! isset($this->rules[$fieldName]) ) {
+            $this->rules[$fieldName] = [];
+        }
+        $this->rules[$fieldName][] = $validateRule;
+    }
+
+    public function getErrors(string $fieldName = null)
+    {
+        if ( is_null($fieldName) ) {
+            return $this->errorsCollection->getAll();
+        }
+        return $this->errorsCollection->getAllByFieldName($fieldName);
+    }
+
+    public function isHasErrors()
+    {
+        return ! $this->errorsCollection->isEmpty();
+    }
+
+    public function getErrorsMessages(string $fieldName)
+    {
+        $errors = $this->getErrors($fieldName);
+        if ( empty($errors) ) {
+            return [];
+        }
+        $errorsMessages = array_map(function($error) {
+            return $error->getMessage();
+        }, $errors);
+        return $errorsMessages;
+    }
+
+    public function getErrorsMessagesAsString(string $fieldName)
+    {
+        $string = '';
+        $errorsMessages = $this->getErrorsMessages($fieldName);
+        if ( empty($errorsMessages) ) {
+            return $string;
+        }
+        return implode(' and ', $errorsMessages);
+    }
 }
--- validators/ArrayValidator.php
+++ validators/ArrayValidator.php
@@ -4,45 +4,24 @@
 namespace abaranchik178\validation\validators;
 use abaranchik178\validation\{
     Validator,
-    ValidateError,
-    ValidateErrorsCollection,
-    ValidateRule
+    ValidateError
 };
 
-class ArrayValidator implements Validator
+class ArrayValidator extends Validator
 {
-    protected $rules;
-    protected $errorsCollection;
-
-    public function __construct()
-    {
-        $this->errorsCollection = new ValidateErrorsCollection();
-    }
-
-    public function addRule(string $fieldName, ValidateRule $validateRule)
-    {
-        if ( ! isset($this->rules[$fieldName]) ) {
-            $this->rules[$fieldName] = [];
-        }
-        $this->rules[$fieldName][] = $validateRule;
-    }
-
     public function validate($data)
     {
         if ( empty($this->rules) ) {
             return true;
         }
-        if ( empty($data) ) {
-            //fixme  should depend on validation rules
-            return false;
-        }
         foreach ($this->rules as $fieldName => $validateRules) {
             foreach ($validateRules as $rule) {
-                //fixme??
                 if ( ! isset($data[$fieldName]) ) {
-                    throw new \LogicException(
-                        "Your data don't have field $fieldName, but you try add validation rule for her"
-                    );
+                    $this->errorsCollection->add($fieldName, new ValidateError(
+                        $fieldName,
+                        null,
+                        "Your data don't have field $fieldName"
+                    ));
                 }
                 if ( ! $rule->isValid($data[$fieldName]) ) {
                     $this->errorsCollection->add($fieldName, new ValidateError(
@@ -57,40 +36,5 @@ class ArrayValidator implements Validator
             return false;
         }
         return true;
-    }
-
-    public function getErrors(string $fieldName = null)
-    {
-        if ( is_null($fieldName) ) {
-            return $this->errorsCollection->getAll();
-        }
-        return $this->errorsCollection->getAllByFieldName($fieldName);
-    }
-
-    public function isHasErrors()
-    {
-        return ! $this->errorsCollection->isEmpty();
-    }
-
-    public function getErrorsMessages(string $fieldName)
-    {
-        $errors = $this->getErrors($fieldName);
-        if ( empty($errors) ) {
-            return [];
-        }
-        $errorsMessages = array_map(function($error) {
-            return $error->getMessage();
-        }, $errors);
-        return $errorsMessages;
-    }
-
-    public function getErrorsMessagesAsString(string $fieldName)
-    {
-        $string = '';
-        $errorsMessages = $this->getErrorsMessages($fieldName);
-        if ( empty($errorsMessages) ) {
-            return $string;
-        }
-        return implode(' and ', $errorsMessages);
     }
 }
